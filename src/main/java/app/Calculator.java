72ba4da6ea5011e3f521bccde1d17ec335caa106
ap2299 <aparnasamant77@gmail.com>
2023-08-01 16:03:01 +0530

    First Commit

---
+++ src/main/java/app/Calculator.java
@@ -0,0 +1,19 @@
+package app;
+
+public class Calculator 
+{
+   public int add(int i, int j)
+   {
+	   return i+j;
+   }
+   
+   public int multiply(int i, int j)
+   {
+	   return i*j;
+   }
+
+   public int divide(int i, int j)
+   {
+	   return i/j;
+   }
+}
---
+++ src/test/java/app/CalculatorTest.java
@@ -0,0 +1,18 @@
+package app;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Test
+	public void testAdd() 
+	{
+		int exp = 30;
+		Calculator cal = new Calculator();
+		int act=cal.add(10,20);
+		assertEquals(exp, act);
+	}
+
+}
