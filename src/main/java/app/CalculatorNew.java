a716ff35c4b7e4c6991eec52c66f09b98153a3e5
ap2299 <aparnasamant77@gmail.com>
2023-08-02 15:46:32 +0530

    A new class added

--- src/main/java/app/CalculatorNew.java
+++ src/main/java/app/CalculatorNew.java
@@ -2,11 +2,13 @@ package app;
 
 public class CalculatorNew 
 {
-
-	public static void main(String[] args)
-	{
-		
-
-	}
-
+	 public int subtract ( int i, int j)
+	   {
+		   if(i>j)
+			   return i-j;
+		   else
+			   return 
+					   j-i;
+	   }
+	
 }
