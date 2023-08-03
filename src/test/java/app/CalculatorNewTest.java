a652dc6cae0679431246a31bf45beee9f57b475c
ap2299 <aparnasamant77@gmail.com>
2023-08-04 01:42:14 +0530

    added a new test2 method

---
+++ src/test/java/app/CalculatorNewTest.java
@@ -0,0 +1,21 @@
+package app;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorNewTest 
+{
+	@Test
+	public void test() 
+	{
+		fail("Not yet implemented");
+	}
+	@Test 
+	   public void test2()
+	   {
+	     System.out.println("A new method added");
+	   } 
+	
+    
+}
