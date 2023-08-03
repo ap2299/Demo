cabd6ae8386a611690b157e5b786f8b21283c2e4
ap2299 <aparnasamant77@gmail.com>
2023-08-04 00:25:35 +0530

    A new second class added

---
+++ src/test/java/app/CalculatorNewTest.java
@@ -0,0 +1,15 @@
+package app;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorNewTest 
+{
+
+	@Test
+	public void test() 
+	{
+		fail("Not yet implemented");
+	}
+}
